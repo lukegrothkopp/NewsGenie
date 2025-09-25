5d5f8e2bca0875e95e02cb52b3921ede1507c8b9
lukegrothkopp <lukegrothkopp@gmail.com>
2025-09-25 12:36:18 -0700

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,1 +1,4 @@
+def __init__(self, api_key: Optional[str] = None, country: str = None):
+    self.api_key = api_key or os.getenv("NEWSAPI_KEY") or os.getenv("NEWS_API_KEY")
+    self.country = (country or os.getenv("NEWSAPI_COUNTRY") or "us").lower()
 
